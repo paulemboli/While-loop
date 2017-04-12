9e32b86bfa39b7905b44da43b87680d40741aaa6
paulemboli <iampaulemboli@gmail.com>
2017-04-12 06:46:52 -0500

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,26 @@
+package Review.java;
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		
+		int numStars = 0;
+		int numPrinted = 0;
+		
+		
+		numStars = 12;
+		
+		numPrinted = 1;
+		
+		while (numPrinted <= numStars){
+			
+			System.out.print(" * ");
+			
+			numStars --;
+			
+		}
+		
+		System.out.println(" ");
+	}
+
+}
